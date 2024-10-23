ca6c2a3e09192c3725365fc0008069237f06db01
chandan maurya <76623223+chandan766@users.noreply.github.com>
2024-10-23 08:48:07 +0530

    Update Pattern8.java

--- java/pattern/Pattern8.java
+++ java/pattern/Pattern8.java
@@ -9,21 +9,16 @@ import java.util.*;
 public class Pattern8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a num: ");
+        System.out.print("Enter the no. of lines: ");
         int n = sc.nextInt();
-        int k = 1;
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=n-i;j++)
-            {
-                System.out.print(" ");
-            }
-            for(int j=1;j<=k;j++)
-            {
-                System.out.print("x");
+        int k = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n+k;j++){
+                if(j<=n-i) System.out.print(" ");
+                else System.out.print("x");
             }
             System.out.println();
-            k+=2;
+            k++;
         }
     }
 }
