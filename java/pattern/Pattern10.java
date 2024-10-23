cb03344e9bbd81e8005e1b563d3d9084265a1310
chandan maurya <76623223+chandan766@users.noreply.github.com>
2024-10-23 09:20:20 +0530

    Update Pattern10.java

--- java/pattern/Pattern10.java
+++ java/pattern/Pattern10.java
@@ -14,7 +14,7 @@ import java.util.*;
 public class Pattern10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the no. of lines: ");
+        System.out.print("Enter an odd no: ");
         int n = sc.nextInt();
         for(int i=1;i<=n;i++)
         {   
