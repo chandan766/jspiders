77dce5dd9dbfcbe9bb72532da476ee2d26f59605
chandan maurya <76623223+chandan766@users.noreply.github.com>
2024-10-23 08:51:14 +0530

    Update Pattern7.java

--- java/pattern/Pattern7.java
+++ java/pattern/Pattern7.java
@@ -15,11 +15,8 @@ public class Pattern7 {
         for(int i=1;i<=n;i++)
         { 
            for(int j=1;j<=n;j++){
-            if(j<=n-i){
-                System.out.print(" ");
-            }else{
-                System.out.print("x");
-            }
+            if(j<=n-i) System.out.print(" ");
+            else System.out.print("x");
            }
             System.out.println();
         }
