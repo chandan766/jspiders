d4638782819055868fbd1019a6f06e63047dfb11
chandan maurya <76623223+chandan766@users.noreply.github.com>
2024-10-23 09:11:58 +0530

    Update Pattern10.java

--- java/pattern/Pattern10.java
+++ java/pattern/Pattern10.java
@@ -14,24 +14,18 @@ import java.util.*;
 public class Pattern10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a num: ");
+        System.out.print("Enter the no. of lines: ");
         int n = sc.nextInt();
         for(int i=1;i<=n;i++)
         {   
             int m = n/2+1;
             for(int j=1;j<=m;j++){
                 if(i<=m){
-                    if (j<=m-i) {
-                        System.out.print(" ");
-                    }else{
-                        System.out.print("x");
-                    }
+                    if (j<=m-i) System.out.print(" ");
+                    else System.out.print("x");
                 }else{
-                    if (j<=i-m) {
-                        System.out.print(" ");
-                    }else{
-                        System.out.print("x");
-                    }
+                    if (j<=i-m) System.out.print(" ");
+                    else System.out.print("x");
                 }
                }
                 System.out.println();
